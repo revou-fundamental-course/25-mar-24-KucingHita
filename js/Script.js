35a57da6df4fa23e515435ce2ba82e74a499c337
Akmal Najib <135620988+KucingHita@users.noreply.github.com>
2024-04-03 14:22:55 +0700

    init commit

---
+++ js/Script.js
@@ -0,0 +1,6 @@
+function peringatan(){
+    var BeratBadan = document.getElementsByClassName("Berat-Badan")
+    var Usia = document.getElementsByClassName("Usia")
+    var TinggiBAdan = document.getElementsByClassName("Tinggi-Badan")
+    console.log();
+}
