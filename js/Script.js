a123b10bbdd47f22f7bf0ac7bd1e51edcd0023fe
Akmal Najib <135620988+KucingHita@users.noreply.github.com>
2024-04-04 18:56:13 +0700

    init commit

--- js/Script.js
+++ js/Script.js
@@ -1,12 +1,40 @@
-function calculate(){
-    let weightinput = document.getElementById('weight-input');
-    console.log(weightinput.value);
+document.getElementById("bmiform").addEventListener("submit" ),function(event){
+  event.preventDefault();
 
-    let ageinput = document.getElementById('age-input');
-    console.log(ageinput.value);
+  var sex = document.querySelector('input[name"sex"]:checked');
+  var berat = parseFloat(document.getElementById("weight-input").value);
+  var tahun = parseFloat(document.getElementById("age-input").value);
+  var tinggi = parseFloat(document.getElementById("height-input").value);
 
-    let heightinput = document.getElementById('height-input');
-    console.log(heightinput.value);
+  // Rumus BMI
+  var bmi = berat / (tinggi * tinggi)
 
-    
- }
+  // Rentang skor Dalam kategori BMI
+  var kategori;
+  if (bmi < 18,5){
+     kategori = "Kurus";
+  }else if (bmi >= 18.5 && bmi < 24.9){
+     kategori = "Normal";
+  }else if (bmi >= 25 && bmi < 29.9){
+     kategori = "Gemuk";
+  }else {
+     kategori = "Obesitas";
+  }
+
+  // Memberikan keterangan
+  var keterangan;
+  switch (kategori){
+      case "Kurus":
+          keterangan = "berat Badan Anda Rendah.";
+          break;
+      case "Normal":
+          keterangan = "Berat Badan Anda Ideal.";
+          break;
+      case "Gemuk":
+          keterangan = "Anda Termasuk Memiliki Berat Berlebih.";
+          break;
+      case "Obesitas":
+          keterangan = "Anda tergolong Obesitas.Segera Konsultasikan dengan dokter untuk mendapatkan saran kesehatan."
+  }
+  
+}
